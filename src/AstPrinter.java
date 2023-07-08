65f9a10cc1f3d4db2ddeca6e2ff73941bc4ba29c
Tim Beck <becktimd@gmail.com>
2023-07-08 13:39:01 +0200

    assignment

--- src/AstPrinter.java
+++ src/AstPrinter.java
@@ -19,6 +19,11 @@ class AstPrinter implements Expr.Visitor<String> {
   }
 
   @Override
+  public String visitAssignExpr(Expr.Assign expr) {
+    return String.format("(assign %s %s)", expr.name, expr.value);
+  }
+
+  @Override
   public String visitVariableExpr(Expr.Variable expr) {
     return String.format("(get %s)", expr.name);
   }
